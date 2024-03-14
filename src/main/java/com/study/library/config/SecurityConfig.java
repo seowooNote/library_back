93d6f34b10eb9c9698cb57ec5d6aff09ba3f22fc
seowooNote <dkr0210@naver.com>
2024-03-14 17:52:53 +0900

    회원가입

--- src/main/java/com/study/library/config/SecurityConfig.java
+++ src/main/java/com/study/library/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.study.library.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity // 3
 @Configuration // 1
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override // 2
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
--- src/main/java/com/study/library/dto/SignupReqDto.java
+++ src/main/java/com/study/library/dto/SignupReqDto.java
@@ -1,11 +1,34 @@
 package com.study.library.dto;
 
+import com.study.library.entity.User;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class SignupReqDto {
+
+    @Pattern(regexp = "^[a-z]{1}[a-z0-9]{2,10}+$", message = "영문 숫자 조합 3~10자리여야 합니다.")
     private String username;
-    private String password;
+    @Pattern(regexp = "^(?=.*[a-zA-Z])((?=.*\\d)|(?=.*\\W)).{7,128}+$", message = "대소문자, 숫자, 특수문자 조합으로 8 ~ 128자리여야 합니다.")
+    private String password; // 1q2w3e4r!!
+    @Pattern(regexp = "^[ㄱ-ㅎ|가-힣]{1,}$", message = "한글이어야 합니다.")
     private String name;
+    @NotBlank
+    @Email(regexp = "^[a-zA-Z0-9]+@[0-9a-zA-Z]+\\.[a-z]+$")
     private String email;
+
+    public User toEntity(BCryptPasswordEncoder passwordEncoder) {
+        return User.builder()
+                .username(username)
+                .password(passwordEncoder.encode(password))
+                .name(name)
+                .email(email)
+                .build();
+    }
 }
